3d13c86538ff5942fc46443a4d17d272c37405f4
lewislovesgames <lewisminecraft@hotmail.co.uk>
2015-02-21 19:24:55 +0000

    Create Server.cs

---
+++ Server.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyNamespace
+{
+    class Server : IServer
+    {
+        public void SetMOTD(string MOTD)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetMaxPlayers(int MaxPlayers)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetOP(string player)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void EnableWhitelist(bool whitelistEnabled)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetWhitelistedPlayers(string[] whitelist)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void StartServer()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void StopServer()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
