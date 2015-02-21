42cdd0889a8064aab9bf0a361d472568c992b859
lewislovesgames <lewisminecraft@hotmail.co.uk>
2015-02-21 19:23:10 +0000

    Update IServer.cs

--- IServer.cs
+++ IServer.cs
@@ -1,1 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
+namespace CSharpCraft
+{
+    interface IServer
+    {
+        public abstract void SetMOTD(String MOTD);
+        public abstract void SetMaxPlayers(int MaxPlayers);
+        public abstract void SetOP(String player);
+        public abstract void EnableWhitelist(Boolean whitelistEnabled);
+        public abstract void SetWhitelistedPlayers(String[] whitelist);
+        public abstract void StartServer();
+        public abstract void StopServer();
+    }
+}
